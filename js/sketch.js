dbd95ae5bc2325dfd1905e91a6ad04a876fda768
Michael Cassens <michaelcassens@gmail.com>
2022-03-13 23:00:15 -0700

    Update sketch.js

--- js/sketch.js
+++ js/sketch.js
@@ -11,9 +11,11 @@ var isGameOver = false;
 var isHit = false;
 var enemies = [];
 var enemySpeeds = [];
-function preload()
-{
-	
+var enemySpeedsX = [];
+var allBehind = false;
+var button;
+function preload() {
+
 }
 
 function windowResized() {
@@ -22,36 +24,38 @@ function windowResized() {
 	myWidth = windowWidth;
 }
 
-function setup()
-{
+function setup() {
 	createCanvas(windowWidth, windowHeight);
-	
+
 	myHeight = windowHeight;
 	myWidth = windowWidth;
-	character = createSprite(myWidth/2,myHeight-67);
-	//addEnemy();
-	enemy = createSprite(myWidth+10, random(myHeight/2,myHeight-67));
-	//console.log(enemy.position.y + ":" + (myWidth+10));
-	enemy.addAnimation("run","./assets/enemy/sprite_0.png", "./assets/enemy/sprite_3.png");
-	enemy.scale+=.5;
+	character = createSprite(myWidth / 2, myHeight - 67);
+	enemy = createSprite(myWidth + 10, random(myHeight / 2, myHeight - 67));
+	enemy.addAnimation("run", "./assets/enemy/sprite_0.png", "./assets/enemy/sprite_3.png");
+	enemy.scale += .5;
 	enemies.push(enemy);
-	enemySpeeds.push(random(1,5));
-	character.addAnimation("run","./assets/character/sprite_0.png", "./assets/character/sprite_5.png");
+	enemySpeeds.push(random(1, 5));
+	enemySpeedsX.push(random(-3,1));
+	character.addAnimation("run", "./assets/character/sprite_0.png", "./assets/character/sprite_5.png");
 }
 
-function draw(){
+function draw() {
 	fullscreen();
-	background(0,0,139);
+	background(0, 0, 139);
 	showCircles();
-	
-	for(let i=0; i < enemies.length; i++)
-	{
+
+	for (let i = 0; i < enemies.length; i++) {
 		enemies[i].changeAnimation("run");
-	
+
 	}
-	if(!isGameOver)
-	{
+	if (!isGameOver) {
 		moveEnemy();
+		if (checkEnemyPosition()) {
+
+			resetEnemies();
+			increaseScore();
+			isHit = false;
+		}
 	}
 	character.changeAnimation("run");
 
@@ -59,172 +63,166 @@ function draw(){
 
 	drawSprites();
 
-	rect(0,myHeight-50,myWidth, 50);
-	
-	var hit = collideRectRect(0, myHeight-50, myWidth, 50, character.position.x, character.position.y, 32, 17);
-	if(hit)
-	{
+	rect(0, myHeight - 50, myWidth, 50);
+
+	var hit = collideRectRect(0, myHeight - 50, myWidth, 50, character.position.x, character.position.y, 32, 17);
+	if (hit) {
 		character.velocity.y = 0;
-		character.position.y = myHeight-67;
-	}	
-	else
-	{
-		if(!isGameOver)
-		character.velocity.y = 1;
+		character.position.y = myHeight - 67;
+	} else {
+		if (!isGameOver)
+			character.velocity.y = 1;
 	}
 
-	for(let i = 0; i < enemies.length; i++)
-	{
+	for (let i = 0; i < enemies.length; i++) {
 		var enemyHit = collideRectRect(enemies[i].position.x, enemies[i].position.y, 32, 32, character.position.x, character.position.y, 32, 32);
-		if(enemyHit)
-		{
-			
+		if (enemyHit) {
+
 			isHit = true;
-			health-=1;
-			if(health <= 0)
-			{
+			health -= 1;
+			if (health <= 0) {
 				gameOver();
 				health = 0;
-			}
-			else
-			{
-				
+			} else {
+
 				character.position.y = -100;
 			}
-			
+
 		}
 	}
-	
+
 
 	textSize(24);
-	text("Score: " + score, myWidth-150, 50);
-	text("Health: " + health, myWidth-150, 100);
-	
+	text("Score: " + score, myWidth - 150, 50);
+	text("Health: " + health, myWidth - 150, 100);
+
 }
 
-function addEnemy()
-{
-	enemy = createSprite(myWidth+10, random(myHeight/2,myHeight-67));
+function addEnemy() {
 
-	enemy.addAnimation("run","./assets/enemy/sprite_0.png", "./assets/enemy/sprite_3.png");
-	enemy.scale+=.5;
+	enemy = createSprite(myWidth + 10, floor(random(myHeight / 2, (myHeight - 67))));
+
+	enemy.addAnimation("run", "./assets/enemy/sprite_0.png", "./assets/enemy/sprite_3.png");
+	enemy.scale += .5;
 	enemies.push(enemy);
+	enemySpeeds.push(random(1, 5));
+	enemySpeedsX.push(random(-3,-1));
+
 }
-function showCircles()
-{
-	for(let i = 0; i < numberOfCircles; i++)
-	{
-		let circleObj = new mycircle(myWidth+50, floor(random(0,myHeight))-100, floor(random(10)+1), myWidth);
+
+function showCircles() {
+	for (let i = 0; i < numberOfCircles; i++) {
+		let circleObj = new mycircle(myWidth + 50, floor(random(0, myHeight)) - 100, floor(random(10) + 1), myWidth);
 		stars.push(circleObj);
 	}
-	for(let i = 0; i < numberOfCircles; i++)
-	{
+	for (let i = 0; i < numberOfCircles; i++) {
 		stars[i].updateCircle();
 		stars[i].drawCircle();
-		
+
 	}
-	for(let k = 0; k < stars.length; k++)
-	{
-		
-		if(stars[k].getX() <=0)
-		{
-			stars.splice(k,1);
+	for (let k = 0; k < stars.length; k++) {
+
+		if (stars[k].getX() <= 0) {
+			stars.splice(k, 1);
 		}
 	}
-	
+
 }
-function gameOver()
-{
-	for(let i = 0; i < enemies.length; i++)
-	{
+
+function gameOver() {
+	for (let i = 0; i < enemies.length; i++) {
 		enemies[i].velocity.x = 0;
 		enemies[i].velocity.y = 0;
-		
+
 	}
 	character.velocity.x = 0;
 	character.velocity.y = 0;
 	numberOfCircles = 0;
 	isGameOver = true;
 	textSize(24);
-	text("Game Over", myWidth/2-75, myHeight/2);
-
+	text("Game Over", myWidth / 2 - 75, myHeight / 2);
+	button = createButton('restart?');
+	button.position(myWidth / 2 - 50, myHeight/2+50);
+	button.mousePressed(restart);
 
 }
-function moveEnemy()
+
+function restart()
 {
-	
-	for(let i = 0; i < enemies.length; i++)
-	{
-			
-		if(enemies[i].position.y >= myHeight-67 || enemies[i].position.y <= myHeight/2)
-		{
+	window.location.reload();
+}
+
+function moveEnemy() {
+
+	for (let i = 0; i < enemies.length; i++) {
+
+		if (enemies[i].position.y >= myHeight - 67 || enemies[i].position.y <= myHeight / 2) {
 			enemySpeeds[i] *= -1;
 		}
-		enemies[i].velocity.x = -1;
+		enemies[i].velocity.x = enemySpeedsX[i];
 		enemies[i].velocity.y = enemySpeeds[i];
-		
-		if(enemies[i].position.x <= 0)
-		{
-			if(!isHit)
-			{
-				score+=1;
-				if(score % 2 == 0)
-				{
-					addEnemy();
-				}
-				
-			}
-			isHit = false;
-			for(let i = 0; i < enemies.length; i++)
-			{
-				enemySpeedY = floor(random(1,5));
-				enemySpeeds[i] = enemySpeedY;
-			}
-			enemies[i].position.x = myWidth + 50;
-			enemies[i].position.y = random(myHeight/2,myHeight-67);
-			character.position.y = myHeight-67;
-			character.position.x = myWidth/2;	
+	}
+}
+
+function checkEnemyPosition() {
+	allBehind = true;
+	for (let i = 0; i < enemies.length; i++) {
+		if (enemies[i].position.x > 0) {
+			allBehind = false;
 		}
 	}
-	
-	
+
+	return allBehind;
+}
+
+function increaseScore() {
+	if (!isHit) {
+		score += 1;
+		if (score % 1 == 0) {
+			addEnemy();
+		}
+		isHit = false;
+	}
+}
+
+function resetEnemies() {
+
+
+	for (let i = 0; i < enemies.length; i++) {
+		enemySpeedY = floor(random(1, 5));
+		enemySpeeds[i] = enemySpeedY;
+		enemySpeedsX[i] = floor(random(-3,-1));
+
+		enemies[i].position.x = myWidth + 50;
+		enemies[i].position.y = random(myHeight / 2, (myHeight - 67));
+		character.position.y = myHeight - 67;
+		character.position.x = myWidth / 2;
+	}
+
+
 }
 
 function mousePressed() {
-	if(mouseX >= 0 && mouseX < 100)
-	{
-		character.velocity.x -=.5;
-	}
+	if (mouseX >= 0 && mouseX < 100) {
+		character.velocity.x -= .5;
+	} else if (mouseX <= myWidth && mouseX > myWidth - 100) {
+		character.velocity.x += .5;
+	} else if (mouseY <= myHeight && mouseY > myHeight - 100) {
 
-	else if(mouseX <= myWidth && mouseX > myWidth-100)
-	{
-		character.velocity.x +=.5;
-	}
-	else if(mouseY <= myHeight && mouseY > myHeight-100)
-	{
-		
-		if(character.position.y >= 200)
-		{
-			character.velocity.y -=20;
+		if (character.position.y >= 200) {
+			character.velocity.y -= 20;
 		}
-	}
-	else if(mouseY >= 0 && mouseY < 100)
-	{
+	} else if (mouseY >= 0 && mouseY < 100) {
 		//character.velocity.y -=.5;
-	}
-	
-	else
-	{
+	} else {
 		character.velocity.x = 0;
 		character.velocity.y = 0;
 	}
 }
 
-  function mouseReleased()
-  {
+function mouseReleased() {
 	character.velocity.x = 0;
-	if(!isGameOver)
-	{
+	if (!isGameOver) {
 		character.velocity.y -= .5;
 	}
 }
